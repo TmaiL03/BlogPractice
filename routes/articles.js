dabb0138d59ea35fc01b222c60e01f9855b3972f
Liam Tingley <liamtingley@gmail.com>
2024-12-22 12:01:43 -0400

    Saving edits to create Article objects

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// 
+const Article = require("../models/article");
 // Creates a router for articles on the blog, in the same way "app" operates in server.js.
 const router = express.Router();
 
@@ -6,13 +8,35 @@ const router = express.Router();
 router.get("/new", (req, res) => {
 
     // Calls the new.ejs view stored in the views/articles subdirectory.
-    res.render("articles/new");
+    res.render("articles/new", { article: new Article()});
 });
 
+// Gets article at specified id.
+router.get("/:id", (req, res) => {
+
+})
+
 // Pulls data from new post form in new.ejs.
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 
+    // Creates Article object in accordance with Article schema.
+    const article = new Article({
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown
+    });
 
+    try {
+
+        article = await article.save();
+        res.redirect(`/articles/${article.id}`);
+
+    } catch (error) {
+
+        // Redirect to new article screen, with prefilled fields from previous submission.
+        res.render("articles/new", { article: article });
+
+    }
 
 });
 
